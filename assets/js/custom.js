6d250e202256880e2c79241648684efb4c1daa5f
VenghongTaing <j.ran1997@gmail.com>
2023-05-29 20:04:29 +0700

    modified:   assets/js/custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -28,6 +28,22 @@ $(document).ready(function () {
     console.error("无法获取导航栏相关元素");
     return;
   }
+
+  // Add this function to check and show the navbar-collapse content
+  function checkWindowWidth() {
+    if (window.innerWidth >= 992) {
+      navbarTarget.classList.add("show");
+
+      var navbarHeight = navbarTarget.clientHeight;
+      var mainElement = document.querySelector("main");
+
+      mainElement.style.marginTop = navbarHeight + "px";
+      navbarTarget.style.maxHeight = navbarTarget.scrollHeight + "px";
+    }
+  }
+
+  checkWindowWidth(); // Check on page load
+
   // 添加事件监听器
   navbarToggler.addEventListener("click", function () {
     navbarTarget.classList.toggle("show");
@@ -47,6 +63,15 @@ $(document).ready(function () {
       navbarTarget.style.transition = "max-height .3s ease-in";
       mainElement.style.marginTop = "";
       navbarTarget.style.maxHeight = "";
+    }
+  });
+
+  // Add this event listener to update on window resize
+  window.addEventListener("resize", function() {
+    if (window.innerWidth >= 992) {
+      checkWindowWidth();
+    } else {
+      navbarTarget.classList.remove("show");
     }
   });
 });
